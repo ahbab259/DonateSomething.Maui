573009bb9a7d0143857c763c52ace2d1e61e216e
ahbab259 <hasin_ahbab@yahoo.com>
2024-11-19 18:55:42 -0500

    Adds organization class and service

---
+++ Models/Organization.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonateSomething.Maui.Models
+{
+    public class Organization
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public string CountryCode { get; set; }
+        public Int64 Budget { get; set; }
+    }
+}
---
+++ Services/OrganizationService.cs
@@ -0,0 +1,28 @@
+﻿using DonateSomething.Maui.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonateSomething.Maui.Services
+{
+    public class OrganizationService
+    {
+        public List<Organization> GetOrganizations()
+        {
+            return new List<Organization>
+            {
+                new Organization
+                {
+                    Id = 1, Name = "OneOrganization", Budget = 100000
+                },
+
+                new Organization
+                {
+                    Id = 2, Name = "TwoOrganization", Budget = 200000
+                }
+            };
+        }
+    }
+}
